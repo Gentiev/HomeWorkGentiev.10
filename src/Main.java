4cd7518ccbc2467e9a5058cd46bed52bdf3e42c0
Dany.Gen <Gentiev@vk.com>
2024-02-03 01:55:51 +0300

    HW.10

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//Задача.1
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = firstName + " " + middleName + " " + lastName;
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
+//Задача.2
+        String fullNameUpperCase = fullName.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета - " + fullNameUpperCase);
+//Задача.3
+        String fullName2 = "Иванов Семён Семёнович";
+        String fullName2Replace = fullName2.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника - " + fullName2Replace);
     }
 }
